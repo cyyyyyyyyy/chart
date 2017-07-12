f22989bcf06c875d9d0bbf0bbe480fb6104c1368
程杨 <610704090@qq.com>
2017-07-12 23:01:17 +0800

    添加egg后台

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1499851873718_2790';
+
+  // add your config here
+
+  return config;
+};
